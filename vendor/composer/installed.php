1f23af8ee9ead66782a6417405a40662e50bdc3f
Patrick Madlang-awa <madlangawapat22@gmail.com>
2023-03-04 14:10:20 +0800

    to railway

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '52df4bda87020400985c948ffc76d9cf3a70e490',
+        'reference' => '0609458bfb2e69ff78e8de0363e979c693d2b4ce',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -421,7 +421,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '52df4bda87020400985c948ffc76d9cf3a70e490',
+            'reference' => '0609458bfb2e69ff78e8de0363e979c693d2b4ce',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
